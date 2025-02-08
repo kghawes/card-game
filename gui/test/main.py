fded1bf50262ac293e96fa21635c99b03175450d
kghawes <36173999+kghawes@users.noreply.github.com>
2025-02-07 23:19:38 -0600

    Add PlayArea widget and enhance card interaction logic

--- gui/test/main.py
+++ gui/test/main.py
@@ -1,14 +1,32 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty
 )
 from kivy.clock import Clock
 from kivy.config import Config
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 
+class PlayArea(Widget):
+    def on_touch_up(self, touch):
+        if super().on_touch_up(touch):
+            return True
+        if not self.collide_point(touch.x, touch.y):
+            return False
+        
+        for child in self.walk():
+            if isinstance(child, Card) and child.is_grabbed:
+                # Do something when a card is grabbed and touch is within PlayArea
+                print("Card is grabbed and touch is within PlayArea")
+                return True
+        
+        return True
+
+
 class Card(Widget):
+    is_grabbed = BooleanProperty(False)  # Add this line
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.click_location = (0, 0)
@@ -22,6 +40,7 @@ class Card(Widget):
         self.click_location = (touch.x, touch.y)
         self.starting_position = (self.center_x, self.center_y)
         touch.grab(self)
+        self.is_grabbed = True  # Update grabbed state
         return True
 
     def on_touch_move(self, touch):
@@ -34,14 +53,16 @@ class Card(Widget):
         return False
     
     def on_touch_up(self, touch):
-        if self.collide_point(*touch.pos):
+        if touch.grab_current is self:
             touch.ungrab(self)
+            self.is_grabbed = False  # Update grabbed state
             return True
         return False
 
 
 class CardGame(Widget):
     card = ObjectProperty(None)
+    play_area = ObjectProperty(None)
     
 
 
