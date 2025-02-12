e015eb4ef1de8201f373f17963a4c2a2c2abe6f4
kghawes <36173999+kghawes@users.noreply.github.com>
2025-02-12 16:35:08 -0600

    remove hover effect and move grabbed card to its own layer

--- gui/main.py
+++ gui/main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.properties import ObjectProperty, BooleanProperty
 from kivy.clock import Clock
 from kivy.core.window import Window
@@ -15,15 +17,17 @@ class PlayArea(Widget):
 class CardPile(Widget):
     pass
 
+class HeldCardLayer(FloatLayout):
+    pass
+
 class Card(Widget):
     is_draggable = BooleanProperty(True)
-    is_grabbed = BooleanProperty(False)
-    is_hovered = BooleanProperty(False)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.click_location = (0, 0)
         self.starting_position = (self.center_x, self.center_y)
+        self.hand_index = 0
 
     def on_touch_down(self, touch):
         if super().on_touch_down(touch):
@@ -33,7 +37,10 @@ class Card(Widget):
         self.click_location = (touch.x, touch.y)
         self.starting_position = (self.center_x, self.center_y)
         touch.grab(self)
-        self.is_grabbed = True
+        self.hand_index = self.parent.children.index(self)
+        held_card_layer = self.parent.get_root_window().children[0].held_card_layer
+        self.parent.remove_widget(self)
+        held_card_layer.add_widget(self)
         return True
 
     def on_touch_move(self, touch):
@@ -48,17 +55,17 @@ class Card(Widget):
         return False
 
     def on_touch_up(self, touch):
-        if touch.grab_current is self or self.is_hovered:
+        if touch.grab_current is self:
             touch.ungrab(self)
-            self.is_grabbed = False
             play_area = self.get_root_window().children[0].play_area
             if play_area.collide_point(self.center_x, self.center_y):
                 self.center_x = play_area.center_x
                 self.center_y = play_area.center_y
                 Clock.schedule_once(self.move_to_discard, 1)
             else:
-                self.center_x = self.starting_position[0]
-                self.center_y = self.starting_position[1]
+                hand = self.get_root_window().children[0].hand
+                self.parent.remove_widget(self)
+                hand.add_widget(self, index=self.hand_index)
             return True
         return False
 
@@ -71,45 +78,37 @@ class Card(Widget):
             discard_pile.remove_widget(discard_pile.children[0])
         discard_pile.add_widget(self)
         self.is_draggable = False
-        self.is_hovered = False
         self.is_grabbed = False
 
 class Hand(BoxLayout):
-    is_card_hovered = BooleanProperty(False)
-
-    def on_touch_up(self, touch):
-        if self.is_card_hovered:
-            for card in self.children:
-                if card.collide_point(touch.x, touch.y) and card.on_touch_up(touch):
-                    if card.is_hovered and not card.collide_point(touch.x, touch.y):
-                        card.is_hovered = False
-                        self.is_card_hovered = False
-                        return True
-        return False
-
-    def on_motion(self, window, pos):
-        for card in self.children:
-            if card.collide_point(pos[0], pos[1]) and not card.is_hovered and not self.is_card_hovered:
-                card.is_hovered = True
-                self.is_card_hovered = True
-                return True
-            if not card.collide_point(pos[0], pos[1]) and card.is_hovered:
-                card.is_hovered = False
-                self.is_card_hovered = False
-        return False
+    pass
 
 class CardGame(Widget):
     play_area = ObjectProperty(None)
     hand = ObjectProperty(None)
     discard_pile = ObjectProperty(None)
+    held_card_layer = ObjectProperty(None)
 
 class CardGameApp(App):
     def build(self):
         game = CardGame()
-        Window.bind(mouse_pos=game.hand.on_motion)
-        game.hand.add_widget(Card())
-        game.hand.add_widget(Card())
-        game.hand.add_widget(Card())
+        card1 = Card()
+        card1_label = Label(text="Card 1", size_hint=(None, None))
+        card1.bind(pos=lambda instance, value: card1_label.setter('pos')(card1, (card1.center_x - card1_label.width / 2, card1.center_y - card1_label.height / 2)))
+        card1.add_widget(card1_label)
+        
+        card2 = Card()
+        card2_label = Label(text="Card 2", size_hint=(None, None))
+        card2.bind(pos=lambda instance, value: card2_label.setter('pos')(card2, (card2.center_x - card2_label.width / 2, card2.center_y - card2_label.height / 2)))
+        card2.add_widget(card2_label)
+        
+        card3 = Card()
+        card3_label = Label(text="Card 3", size_hint=(None, None))
+        card3.bind(pos=lambda instance, value: card3_label.setter('pos')(card3, (card3.center_x - card3_label.width / 2, card3.center_y - card3_label.height / 2)))
+        card3.add_widget(card3_label)
+        game.hand.add_widget(card1)
+        game.hand.add_widget(card2)
+        game.hand.add_widget(card3)
         
         return game
 
