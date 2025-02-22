a7ff92eeac3d9663db188457c60d70470cfc2d56
kghawes <36173999+kghawes@users.noreply.github.com>
2025-02-22 01:30:24 -0600

    separate card gui logic to its own module

--- gui/combat_screen.py
+++ gui/combat_screen.py
@@ -11,15 +11,18 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.config import Config
 import gui_constants as constants
+from card import Card
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 Config.set('graphics', 'resizable', '0')
 
 Builder.load_file('card.kv')
 
+
 class PlayArea(Widget):
     """Widget representing the play area where cards can be played."""
     pass
+
 
 class CardPile(Widget):
     """Widget representing a pile of cards."""
@@ -35,9 +38,11 @@ class CardPile(Widget):
             Color(rgba=[1, 1, 1, 1])
             Rectangle(pos=cardback.pos, size=cardback.size, source='assets/cardback.png')
 
+
 class AnimationLayer(FloatLayout):
     """Layer for handling animations or widgets that need to be temporarily on top of the z-stack."""
     pass
+
 
 class Hand(FloatLayout):
     """Widget representing the player's hand of cards."""
@@ -67,86 +72,6 @@ class Hand(FloatLayout):
         self.remove_widget(card)
         self.position_cards()
 
-class Card(Widget):
-    """Widget representing a card."""
-    is_draggable = BooleanProperty(True)
-    border_color = ColorProperty([0, 0, 0, 0])
-    resource_cost_color = ColorProperty([0, 0, 0, 0])
-    cost_indicator = ObjectProperty([0, 0, 0, 0, 0, 0])
-    resource_cost_alpha = NumericProperty(1)
-    card_type = StringProperty('CARD_BACK')
-
-    def __init__(self, card_data, **kwargs):
-        """Initializes a card with the given data."""
-        super().__init__(**kwargs)
-        self.click_location = (0, 0)
-        self.starting_position = (self.center_x, self.center_y)
-        self.hand_index = 0
-        self.card_type = card_data['type']
-        self.border_color = constants.CARD_TYPE_COLORS[self.card_type]['border']
-        self.resource_cost_color = constants.RESOURCE_COLORS[constants.CARD_TYPE_COLORS[self.card_type]['resource']]
-        self.cost_indicator = constants.RESOURCE_INDICATOR_OFFSETS[constants.CARD_TYPE_COLORS[self.card_type]['indicator']]
-        self.resource_cost_alpha = constants.CARD_TYPE_COLORS[self.card_type]['indicator_transparency']
-
-    def on_touch_down(self, touch):
-        """When clicked, pick up the card."""
-        if super().on_touch_down(touch):
-            return True
-        if not self.is_draggable or not self.collide_point(touch.x, touch.y):
-            return False
-        self.click_location = (touch.x, touch.y)
-        self.starting_position = (self.center_x, self.center_y)
-        touch.grab(self)
-        self.hand_index = self.parent.children.index(self)
-        animation_layer = self.parent.get_root_window().children[0].animation_layer
-        hand = self.parent
-        for card in hand.children:
-            card.is_draggable = False
-        hand.remove_from_hand(self)
-        animation_layer.add_widget(self)
-        return True
-
-    def on_touch_move(self, touch):
-        """Drag the card with the mouse."""
-        if touch.grab_current is self:
-            offset = tuple(Vector(*touch.pos) - Vector(*self.click_location))
-            new_position = tuple(Vector(*self.starting_position) + Vector(*offset))
-            self.center_x = max(self.width / 2, min(new_position[0], Window.width - self.width / 2))
-            self.center_y = max(self.height / 2, min(new_position[1], Window.height - self.height / 2))
-            return True
-        return False
-
-    def on_touch_up(self, touch):
-        """Release a held card."""
-        if touch.grab_current is self:
-            touch.ungrab(self)
-            play_area = self.get_root_window().children[0].play_area
-            if play_area.collide_point(self.center_x, self.center_y):
-                self.center_x = play_area.center_x
-                self.center_y = play_area.center_y
-                Clock.schedule_once(self.move_to_discard, 1)
-            else:
-                hand = self.get_root_window().children[0].hand
-                self.parent.remove_widget(self)
-                hand.add_to_hand(self, index=self.hand_index)
-                for card in hand.children:
-                    card.is_draggable = True
-            return True
-        return False
-
-    def move_to_discard(self, dt):
-        """Moves the card to the discard pile."""
-        discard_pile = self.get_root_window().children[0].discard_pile
-        self.center_x = discard_pile.center_x
-        self.center_y = discard_pile.center_y
-        self.parent.remove_widget(self)
-        if discard_pile.children:
-            discard_pile.remove_widget(discard_pile.children[0])
-        discard_pile.add_widget(self)
-        self.is_draggable = False
-        hand = self.get_root_window().children[0].hand
-        for card in hand.children:
-            card.is_draggable = True
 
 class CardGame(Widget):
     """Main widget for the card game."""
