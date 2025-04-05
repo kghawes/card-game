4cc52cfca75aa8085b346d04396c36229b3e77bd
kghawes <36173999+kghawes@users.noreply.github.com>
2025-04-05 15:04:21 -0500

    Create tooltips.py

---
+++ gui/tooltips.py
@@ -0,0 +1,11 @@
+from kivy.uix.floatlayout import FloatLayout
+
+class Tooltip(FloatLayout):
+    """Tooltip class to display a tooltip in the GUI.  """
+    
+    def __init__(self, **kwargs):
+        """
+        Initialize a new Tooltip.
+        """
+        super().__init__(**kwargs)
+        # TODO
