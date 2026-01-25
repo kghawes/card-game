a6d5a9cd109b636a90047ac87542a8e2eeaefee0
Kenneth <36173999+kghawes@users.noreply.github.com>
2026-01-24 22:54:46 -0500

    Create damage_calculator.py

---
+++ gameplay/damage_calculator.py
@@ -0,0 +1,8 @@
+class DamageCalculator:
+    def calculate_damage(
+            self, defender, attacker, amount, damage_type, status_registry
+            ) -> int:
+        """
+        Calculate the final damage amount after applying statuses and attributes.
+        """
+        pass
